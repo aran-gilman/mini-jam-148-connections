1e4051f23cc6d927d929e7b2de2e97c8689ac2e4
Aran Gilman <f.zoniventris@gmail.com>
2023-12-21 23:06:32 -0800

    Spawn ConnectionRange objects

--- Assets/Scripts/Connector.cs
+++ Assets/Scripts/Connector.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Connector : MonoBehaviour
 {
@@ -11,6 +10,21 @@ public class Connector : MonoBehaviour
     private float _connectionRange = 1.0f;
 
     [SerializeField]
-    [Tooltip("Child object spawned on start that displays the connector's range and provides a collider for checking proximity to other connectors.")]
+    [Tooltip("Child object spawned on awake that displays the connector's range and provides a collider for checking proximity to other connectors.")]
     private GameObject _rangePrefab;
+
+    private Collider2D _rangeCollider;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(_rangePrefab, "RangePrefab must be non-null.");
+        GameObject go = Instantiate(_rangePrefab, transform);
+        _rangeCollider = go.GetComponent<Collider2D>();
+        Assert.IsNotNull(_rangeCollider, "RangePrefab must have a Collider2D component.");
+
+        // The range is a radius and the scale is effectively the diameter,
+        // so set the scale to double the range.
+        go.transform.localScale = new Vector3(
+            _connectionRange * 2.0f, _connectionRange * 2.0f, 1.0f);
+    }
 }
