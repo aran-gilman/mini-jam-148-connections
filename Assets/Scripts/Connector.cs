3fab51f5fbb01e318e888a982afe6a1f8da6472f
Aran Gilman <f.zoniventris@gmail.com>
2023-12-21 22:11:53 -0800

    Create Connector component & test scene

---
+++ Assets/Scripts/Connector.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connector : MonoBehaviour
+{
+    [SerializeField]
+    private int _maxConnections = 2;
+
+    [SerializeField]
+    private float _connectionRange = 1.0f;
+}
