cb848c979ec56f3f342fd92a347942e3703d5fb6
Aran Gilman <f.zoniventris@gmail.com>
2023-12-22 13:14:10 -0800

    Add ShootAbility component

---
+++ Assets/Scripts/ShootAbility.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ShootAbility : MonoBehaviour
+{
+    [SerializeField]
+    private float _range = 1.0f;
+
+    [SerializeField]
+    private float _attackPower = 1.0f;
+}
