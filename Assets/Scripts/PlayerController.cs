7aa934039b1ddaf80b6c3e56682e916b1adfeede
Aran Gilman <f.zoniventris@gmail.com>
2023-12-22 09:33:32 -0800

    Convert clicked screen position to world position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
+    private Camera _mainCamera;
     private Vector3 _currentPointerPosition;
 
     private void OnPlaceStructure()
     {
-        Debug.Log($"OnPlaceStructure() called at position {_currentPointerPosition}");
+        Vector3 worldPos = _mainCamera.ScreenToWorldPoint(_currentPointerPosition);
+        worldPos.z = 0;
+        Debug.Log($"OnPlaceStructure() called at position {worldPos}");
     }
 
     private void OnPointWorld(InputValue value)
     {
         _currentPointerPosition = value.Get<Vector2>();
+    }
+
+    private void Awake()
+    {
+        // We cache this to avoid the cost of looking up the camera every time.
+        _mainCamera = Camera.main;
     }
 }
