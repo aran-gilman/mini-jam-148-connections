9a825850a056a4571c5606b08321f1da0d0bd2e8
Aran Gilman <f.zoniventris@gmail.com>
2023-12-22 09:31:00 -0800

    Get screen position of click event

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,15 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
+    private Vector3 _currentPointerPosition;
+
     private void OnPlaceStructure()
     {
-        Debug.Log("OnPlaceStructure() called");
+        Debug.Log($"OnPlaceStructure() called at position {_currentPointerPosition}");
+    }
+
+    private void OnPointWorld(InputValue value)
+    {
+        _currentPointerPosition = value.Get<Vector2>();
     }
 }
