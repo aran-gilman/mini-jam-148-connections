d1395b644fcb3757f57d129ee9cdde2fccbc3fee
Aran Gilman <f.zoniventris@gmail.com>
2023-12-23 21:12:02 -0800

    Add DisplayName to HoverInfo

--- Assets/Scripts/HoverInfo.cs
+++ Assets/Scripts/HoverInfo.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 public class HoverInfo : MonoBehaviour
 {
     [SerializeField]
+    private string _displayName;
+    public string DisplayName => _displayName;
+
+    [SerializeField]
     [TextArea]
     private string _description;
     public string Description => _description;
