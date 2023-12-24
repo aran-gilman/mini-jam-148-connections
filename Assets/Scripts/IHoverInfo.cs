ad7624e8635aca46af2d20ed51e37f9733d76cf8
Aran Gilman <f.zoniventris@gmail.com>
2023-12-23 22:32:30 -0800

    Add instructions for adding hover info to an object in IHoverInfo

--- Assets/Scripts/IHoverInfo.cs
+++ Assets/Scripts/IHoverInfo.cs
@@ -1,3 +1,8 @@
+// To add hover info to a game object:
+//  1) Create a new child object.
+//  2) Add a Collider2D (e.g. BoxCollider2D).
+//  3) Add a component deriving from IHoverInfo.
+//  4) Set the object's layer to HoverInfo.
 public interface IHoverInfo
 {
     string DisplayName { get; }
