b86426ee8c5510d7d362dfb47fff8bb755b2ce5f
Aran Gilman <f.zoniventris@gmail.com>
2023-12-23 22:22:55 -0800

    Show info of selected structure in Shop

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -82,19 +82,22 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 screenPos = value.Get<Vector2>();
         _currentPointerPosition = _mainCamera.ScreenToWorldPoint(screenPos);
-        Collider2D hoverInfoCollider =
-            Physics2D.OverlapPoint(_currentPointerPosition, _hoverInfoLayer.value);
-        if (hoverInfoCollider != null
-            && hoverInfoCollider.TryGetComponent(out IHoverInfo hoverInfo))
+        if (_currentPlaceable == null)
         {
-            if (hoverInfo != _hoverInfoDisplay.InfoSource)
+            Collider2D hoverInfoCollider =
+                Physics2D.OverlapPoint(_currentPointerPosition, _hoverInfoLayer.value);
+            if (hoverInfoCollider != null
+                && hoverInfoCollider.TryGetComponent(out IHoverInfo hoverInfo))
             {
-                _hoverInfoDisplay.InfoSource = hoverInfo;
+                if (hoverInfo != _hoverInfoDisplay.InfoSource)
+                {
+                    _hoverInfoDisplay.InfoSource = hoverInfo;
+                }
             }
-        }
-        else
-        {
-            _hoverInfoDisplay.InfoSource = null;
+            else
+            {
+                _hoverInfoDisplay.InfoSource = null;
+            }
         }
         _currentPointerPosition.z = 0;
         _currentPointerPosition = SnapToGrid(_currentPointerPosition);
